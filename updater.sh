482b0edc47a69982dd6ddaa69bfaa5bce62be4e7
Shivank Chhaya <shivank.chhaya@gmail.com>
2025-01-23 13:38:56 -0800

    updated filedict in `updater.sh`

--- updater.sh
+++ updater.sh
@@ -4,6 +4,8 @@ files["u-v"]="main.py"
 files["water-gun-war"]="main.py"
 files["text-village-game"]="buildings.py main.py troops.py"
 files["cpp-card-game"]="card-game.cpp"
+files["type-teams"]="enemies/circuitizer.py enemies/massdark.py teams/hot/__init__.py teams/secreteam/__init__.py teams/storm/__init__.py teams/transform/__init__.py teams/__init__.py main.py"
+files["me-and-others"]="main.py"
 for file in ${files[$1]}; do
         curl "https://raw.githubusercontent.com/shibby360/replit-text-games/refs/heads/main/$1/$file" > temp.txt
         if cmp -s temp.txt $1/$file; then
